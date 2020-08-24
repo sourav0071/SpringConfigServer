fe884fb49d162a42af8c2da41ccef9532031000d
unknown <sourav0071@gmail.com>
2020-08-24 06:55:49 -0700

    first commit

---
+++ config-server/src/test/java/com/skg/srv/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skg.srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
